803d0ec1f57dd87828f1efa95c3ffd3297e6cfea
Josh Creek <jcreek93@gmail.com>
2021-08-29 22:31:17 +0100

    feat(*): Add File model

---
+++ FileRepository/Models/File.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace FileRepository.Models
+{
+    public class File
+    {
+        public string Id { get; set; }
+        public string FileName { get; set; }
+        public Stream Content { get; set; }
+        public DateTime Created { get; set; }
+
+        public File GenerateFile(string id, string fileName, Stream content)
+        {
+            File newFile = new File()
+            {
+                Id = id,
+                FileName = fileName,
+                Content = content,
+                Created = DateTime.Now
+            };
+
+            return newFile;
+        }
+    }
+}
