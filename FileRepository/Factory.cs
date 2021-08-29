57acb1d65244abd49e6996a8e4ef773ca0a18da6
Josh Creek <jcreek93@gmail.com>
2021-08-29 23:29:57 +0100

    feat(*): Add base Factory class

---
+++ FileRepository/Factory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FileRepository
+{
+    /// <summary>
+    /// A factory to enable consumers of this package to easily get a specific type of repository.
+    /// </summary>
+    public class Factory
+    {
+    }
+}
